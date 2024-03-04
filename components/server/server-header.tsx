c6744364fd7eb82cf347a7cae5c6dae0dab794d9
AguuSz <agus.sepu2000@hotmail.com>
2024-03-04 15:00:06 -0300

    added create-channel, delete and leave server functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -65,20 +65,26 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 					</DropdownMenuItem>
 				)}
 				{isModerator && (
-					<DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+					<DropdownMenuItem
+						onClick={() => onOpen("createChannel", { server })}
+						className="px-3 py-2 text-sm cursor-pointer">
 						Create channel
 						<PlusCircle className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
 				)}
 				{isModerator && <DropdownMenuSeparator />}
 				{isAdmin && (
-					<DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+					<DropdownMenuItem
+						onClick={() => onOpen("deleteServer", { server })}
+						className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
 						Delete server
 						<Trash className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
 				)}
 				{!isAdmin && (
-					<DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+					<DropdownMenuItem
+						onClick={() => onOpen("leaveServer", { server })}
+						className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
 						Leave server
 						<LogOut className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
